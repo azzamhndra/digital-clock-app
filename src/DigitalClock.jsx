f41c29366fe9c58a7cf51e9ae7c3c1809e8ea5df
Azza Mahendra <176836552+azzamhndra@users.noreply.github.com>
2025-11-28 17:07:06 +0700

    feat: add clock UI layout

---
+++ src/DigitalClock.jsx
@@ -0,0 +1,11 @@
+const DigitalClock = () => {
+  return (
+    <div className="clock-container">
+      <p className="date">15 APRIL 2003</p>
+      <span className="clock">00:00:00</span>
+      <p className="day">SATURDAY</p>
+    </div>
+  );
+};
+
+export default DigitalClock;
